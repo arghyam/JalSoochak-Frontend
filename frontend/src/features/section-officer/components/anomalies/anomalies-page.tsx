a985ed9c13d559340f24af70c0da845099148078
mukuljakhar-bit <mukul.jakhar@beehyv.com>
2026-05-22 19:50:18 +0530

    chore: small change

--- frontend/src/features/section-officer/components/anomalies/anomalies-page.tsx
+++ frontend/src/features/section-officer/components/anomalies/anomalies-page.tsx
@@ -260,7 +260,7 @@ export function AnomaliesPage() {
             setPage(1)
           }}
           placeholder={t('pages.anomalies.filterDuration')}
-          width="160px"
+          width="206px"
           height="32px"
           borderRadius="4px"
           fontSize="sm"
